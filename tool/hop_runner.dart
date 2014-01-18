51b95eb897f67455bba5fe26111e42c1d0a36e28
Vadim Tsushko <vadimtsishko@gmail.com>
2014-01-18 17:26:41 +0600

    hop documentation generator setup

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -6,7 +6,9 @@ import 'package:hop/hop_tasks.dart';
 void main(args) {
 
   var paths = ['lib/objectory_browser.dart','lib/objectory_console.dart','bin/objectory_server.dart'];
-  addTask('docs',createDartDocTask(paths, linkApi: true, excludeLibs: ['fixnum','mongo_dart_query','objectory']));
+  addTask('docs',createDartDocTask(paths, linkApi: true, excludeLibs: ['fixnum','mongo_dart_query',
+  'args','mongo_dart','args.src.parser','args.src.usage','crypto',
+  'dart.pkg.collection.wrappers','logging','metadata','options','unmodifiable_collection','objectory']));
   paths = ['lib/objectory.dart','example/blog.dart']; //etc etc etc
   addTask('analyze_libs', createAnalyzerTask(paths));
 
